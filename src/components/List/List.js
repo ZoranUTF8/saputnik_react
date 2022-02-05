9b1aa532565d74107216cf1c95b94d959af493ac
ZoranUTF8 <janjiczoran23@gmail.com>
2022-02-05 17:10:50 +0100

    add list frontend

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState } from "react";
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+
+import useStyles from "./styles";
 
 const List = () => {
-  return <div>list</div>;
+  const classes = useStyles();
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
+  return (
+    <>
+      <div className={classes.container}>
+        <Typography variant="h4">
+          Restorani, hoteli i atrakcije oko vas
+        </Typography>
+        <FormControl className={classes.formControl}>
+          <InputLabel>Tip</InputLabel>
+          <Select value={type} onChange={(evt) => setType(evt.target.value)}>
+            <MenuItem value="restaurants">Restorani</MenuItem>
+            <MenuItem value="hotels">Hoteli</MenuItem>
+            <MenuItem value="attractions">Atrakcije</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel>Ocjena</InputLabel>
+          <Select value={rating} onChange={(evt) => setRating(evt.target.value)}>
+            <MenuItem value={0}>Svi</MenuItem>
+            <MenuItem value={3}>Iznad 3.0</MenuItem>
+            <MenuItem value={4}>Iznad 4.0</MenuItem>
+            <MenuItem value={4.5}>Iznad 4.5</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+    </>
+  );
 };
 
 export default List;
